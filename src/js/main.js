ea8eed1afea700d6997cab8c17f5ccb301713929
Romnios <romanios108@gmail.com>
2022-07-27 17:22:56 +0400

    third commit add mobile menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,17 @@
 
-console.log("Hello!");
+//mobile-menu
+
+const mobileBtn = document.querySelector('.header__open-btn');
+const mobileMenu = document.querySelector('.header__nav');
+const btnChange = document.querySelector('.header__open-el');
+// const mobileWedo = document.querySelector('.wedo');
+// const mobiSwiperTop = document.querySelector('.swiperTop');
+
+mobileBtn.addEventListener('click', function() {
+  mobileMenu.classList.toggle('active');
+  mobileBtn.classList.toggle('active');
+  btnChange.classList.toggle('active');
+
+  // mobileWedo.classList.toggle('mobile');
+  // mobiSwiperTop.classList.toggle('hidden');
+})
