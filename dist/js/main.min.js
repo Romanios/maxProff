1c867b829633a40916cba2846720c6a2e021ff3e
Romnios <romanios108@gmail.com>
2022-08-13 20:16:46 +0400

    add scroll-to-top

--- dist/js/main.min.js
+++ dist/js/main.min.js
@@ -1,4 +1,5 @@
 
+
 //mobile-menu
 
 const mobileBtn = document.querySelector('.header__open-btn');
@@ -154,3 +155,45 @@ for(let anchor of anchors) {
     })
   })
 }
+
+// scroll-to-top
+const offset = 100; //Прячет кнопку при скролле менее 100px
+const scrollUp = document.querySelector('.scroll-up');
+const scrollUpSvgPath = document.querySelector('.scroll-up__path');
+const pathLength = scrollUpSvgPath.getTotalLength();
+
+scrollUpSvgPath.style.strokeDasharray = `${pathLength} ${pathLength}`;
+scrollUpSvgPath.style.transition = 'stroke-dashoffset 20ms';
+
+
+const getTop = () => window.pageYOffset || document.documentElement.scrollTop;
+
+// updateDashoffset
+const updateDashoffset = () => {
+  const height = document.documentElement.scrollHeight - window.innerHeight;
+  const dashoffset = pathLength - (getTop() * pathLength / height);
+
+  scrollUpSvgPath.style.strokeDashoffset = dashoffset;
+};
+
+
+// onScroll
+window.addEventListener('scroll', () => {
+  // const updateDashoffset();
+
+  if (getTop() > offset) {
+    scrollUp.classList.add('scroll-up--active');
+  } else {
+    scrollUp.classList.remove('scroll-up--active');
+  }
+});
+// click
+scrollUp.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
+
+
+
